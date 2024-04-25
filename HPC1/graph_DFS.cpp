416b76b1e1b771ace582015388df35c1368b6092
Kaveri Raut <97447480+KaveriRaut@users.noreply.github.com>
2024-04-25 18:35:46 +0530

    Update graph_DFS.cpp

--- HPC1/graph_DFS.cpp
+++ HPC1/graph_DFS.cpp
@@ -10,28 +10,38 @@ vector<int> graph[MAX];
 bool visited[MAX];
 
 void dfs(int node) {
-	stack<int> s;
-	s.push(node);
+    stack<int> s;
+    s.push(node);
 
-	while (!s.empty()) {
-    	int curr_node = s.top();
+    while (!s.empty()) {
+        int curr_node;
+        #pragma omp parallel shared(s) private(curr_node)
+        {
+            #pragma omp critical
+            {
+                curr_node = s.top();
+                s.pop();
+            }
 
-    	if (!visited[curr_node]) {
-        	visited[curr_node] = true;
-		
-    	s.pop();
-	cout<<curr_node<<" ";
+            if (!visited[curr_node]) {
+                visited[curr_node] = true;
+                cout << curr_node << " ";
 
-        	#pragma omp parallel for
-        	for (int i = 0; i < graph[curr_node].size(); i++) {
-            	int adj_node = graph[curr_node][i];
-            	if (!visited[adj_node]) {
-                	s.push(adj_node);
-            	}
-        	}
-    	}
-	}
+                #pragma omp for
+                for (int i = 0; i < graph[curr_node].size(); i++) {
+                    int adj_node = graph[curr_node][i];
+                    #pragma omp critical
+                    {
+                        if (!visited[adj_node]) {
+                            s.push(adj_node);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
+
 
 int main() {
 	int n, m, start_node;
@@ -76,4 +86,15 @@ Enter pair of node and edges:
 0 2
 2 4
 0 2 4 1 
+*/
+
+
+
+/*
+#pragma omp parallel creates a parallel region.
+#pragma omp critical is used to ensure that only one thread at a time accesses and modifies the stack s.
+Inside the parallel region, each thread will pop a node from the stack if it's not empty and then process it if it hasn't been visited.
+If the node hasn't been visited, it will be marked as visited, printed, and its adjacent nodes will be pushed onto the stack if they haven't been visited.
+This approach ensures that each thread operates on its own private copy of curr_node, preventing data races.
+The critical section around accessing and modifying the stack s ensures that only one thread at a time modifies the stack to avoid inconsistencies.
 */
